e0941c3a9108bd2d7f16a8501c57c704a1745a68
ndionis <ndionis@k1r3p7.42mulhouse.fr>
2022-05-10 20:57:45 +0200

    list struct changed (using void * for them items)

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: supersko <supersko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 17:24:45 by supersko          #+#    #+#             */
-/*   Updated: 2022/05/10 18:34:09 by supersko         ###   ########.fr       */
+/*   Updated: 2022/05/10 20:56:51 by supersko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,32 @@
 
 typedef struct s_lnk
 {
+	void			*lnk_items;
+	struct s_lnk	*next;
+	struct s_lnk	*prev;
+}	t_lnk;
+
+typedef struct s_lnk_items
+{
 	int				nb;
 	int				ind;
 	int				indinlist;
 	int				rank;
 	int				rankinlist;
-	struct s_lnk	*next;
-	struct s_lnk	*prev;
-}	t_lnk;
+}	t_lnk_items;
 
 typedef struct s_lst
 {
 	t_lnk	*first;
 	t_lnk	*last;
+	void	*lnk_items;
+}	t_lst;
+
+typedef struct s_lst_items
+{
 	size_t	size;
 	int		min_val;
 	int		max_val;
-}	t_lst;
-
-int		get_args(int argc, char *argv[]);
+}	t_lst_items;
 
 #endif
