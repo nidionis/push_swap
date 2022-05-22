b4f51aac1a547401cee1af449a86c70eece583c8
presk0 <91475081+presk0@users.noreply.github.com>
2022-05-23 00:41:38 +0200

    more efficient when calculating

--- algo.c
+++ algo.c
@@ -12,27 +12,6 @@
 
 #include "push_swap.h"
 
-int best_instr(int lst[6])
-{
-    int i;
-    int lowest;
-    int instr_lowest;
-
-    i = 1;
-    lowest = lst[0];
-    instr_lowest = ra;
-    while (i < 6)
-    {
-        if (lst[i] < lowest)
-        {
-            lowest = lst[i];
-            instr_lowest = i + ra;
-        }
-        i++;
-    }
-    return (instr_lowest);
-}
-
 int can_push(t_lnk lst_a, t_lnk lst_b)
 {
     if (lst_a.rank < lst_b.rank && (lst_a.next).rank > lst_b.rank)
@@ -43,27 +22,34 @@ int can_push(t_lnk lst_a, t_lnk lst_b)
 // return the best instr for reaching
 int shortest_insert(t_lnk *lst_a, t_lnk *lst_b)
 {
-    int     results[6];
+    int     best results[2];
     int     instr;
     int     steps;
     t_lnk   *lst_a_init;
     t_lnk   *lst_b_init;
 
     instr = ra;
+    best_result[1] = INT_MAX;
     while (instr <= rrr)
     {
         steps = 0;
         while (!can_push(*lst_a, *lst_b))
         {
             apply_instr(instr, lst_a, lst_b, 0);
+            if (steps > best_results[1])
+                break;
             steps++;
         }
-        results[instr - ra] = steps;
+        if (steps < best_result[1])
+        {
+            best_result[0] = instr;
+            best_result[1] = steps;
+        }
         lst_a = lst_a_init;
         lst_b = lst_b_init;
         instr++;
     }
-    return (best_instr(results));
+    return (best_result[0]);
 }
 
 void    b_dump(t_lnk **lst_a, t_lnk **lst_b)
