5ab4a8c9d648bd8bdad413adcd3367817cc6b71b
ndionis <ndionis@k1r3p7.42mulhouse.fr>
2022-05-23 23:08:20 +0200

    fonction buggee min_in_dir buguee, itm_insert exploitable

--- algo.c
+++ algo.c
@@ -6,7 +6,7 @@
 /*   By: supersko <supersko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 17:24:45 by supersko          #+#    #+#             */
-/*   Updated: 2022/05/23 13:03:10 by supersko         ###   ########.fr       */
+/*   Updated: 2022/05/23 22:54:05 by supersko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int can_push(t_lnk *lst_a, t_lnk *lst_b)
 }
 
 // return the best instr for reaching
-int *shortest_insert(t_lnk *lst_a, t_lnk *lst_b, int *best_result)
+int *itm_insert(t_lnk *lst_a, t_lnk *lst_b, int *best_result)
 {
     int     instr;
     int     steps;
@@ -30,44 +30,103 @@ int *shortest_insert(t_lnk *lst_a, t_lnk *lst_b, int *best_result)
     instr = ra;
 	lst_a_init = lst_a;
 	lst_b_init = lst_b;
-    best_result[1] = 2147483647;
-    while (instr <= rrr)
-    {
-        steps = 0;
-        while (!can_push(lst_a, lst_b))
-        {
-            apply_instr(instr, &lst_a, &lst_b, 0);
-            if (steps > best_result[1])
-                break;
-            steps++;
-        }
-        if (steps < best_result[1])
-        {
-            best_result[0] = instr;
-            best_result[1] = steps;
-        }
-        lst_a = lst_a_init;
-        lst_b = lst_b_init;
-        instr++;
-    }
-    return (best_result);
+	if (!can_push(lst_a, lst_b))
+	{
+    	while (instr <= rrr)
+    	{
+    	    steps = 0;
+    	    while (!can_push(lst_a, lst_b))
+    	    {
+    	        apply_instr(instr, &lst_a, &lst_b, 0);
+    	        if (steps >= best_result[1])
+    	            break;
+    	        steps++;
+    	    }
+    	    if (steps < best_result[1])
+    	    {
+    	        best_result[0] = instr;
+    	        best_result[1] = steps;
+    	    }
+    	    lst_a = lst_a_init;
+    	    lst_b = lst_b_init;
+    	    instr++;
+    	}
+	}
+	return (best_result);
+}
+
+int	min_in_dir(t_lnk *lst_a, t_lnk *lst_b, int instr, int *best_inst_step)
+{
+    t_lnk	**lst_a_cpy;
+    t_lnk	**lst_b_cpy;
+	int		steps;
+	int 	*best_at_step;
+
+	lst_a_cpy = &lst_a;
+	lst_b_cpy = &lst_b;
+	best_at_step = malloc(sizeof(int) * 3);
+	if (!best_at_step)
+		error_msg("[min_in_dir] malloc error");
+	best_at_step[2] = best_inst_step[1];
+	steps = 1;
+	while (steps < best_at_step[2])
+	{
+		apply_instr(instr, lst_b_cpy, lst_b_cpy, 0);
+		best_at_step = itm_insert(*lst_b_cpy, *lst_b_cpy, best_at_step);
+		if (best_at_step[1] < best_inst_step[1] - steps)
+		{
+			best_at_step[2] = best_at_step[1] + steps;
+		}
+		steps++;
+	}
+	if (best_at_step[2] < best_inst_step[1])
+		best_inst_step[1] = best_at_step[2];
+	free(best_at_step);
+	return (steps);
 }
 
 void    b_dump(t_lnk **lst_a, t_lnk **lst_b)
 {
-    int *instr;
+    int *best_instr_step;
+    int min;
+    int instr;
 
-	instr = malloc(2 * sizeof(int));
-	if (!instr)
+	best_instr_step = malloc(2 * sizeof(int));
+	if (!best_instr_step)
 		error_msg("[b_dump] error malloc");
     while (*lst_b)
     {
-        instr = shortest_insert(*lst_a, *lst_b, instr);
-        while (!can_push(*lst_a, *lst_b))
-        {
-            apply_instr(instr[0], lst_a, lst_b, 1);
-        }
-        apply_instr(pa, lst_a, lst_b, 1);
+		best_instr_step[1] = 0;
+		if (!can_push(*lst_a, *lst_b))
+		{
+			best_instr_step[1] = 2147483647;
+  	    	best_instr_step = itm_insert(*lst_a, *lst_b, best_instr_step);
+			instr = ra;
+			min = 2147483647;
+			while (instr <= rrr)
+			{
+				min_in_dir(*lst_a, *lst_b, instr, best_instr_step);
+				min = best_instr_step[1];
+				if (best_instr_step[1] > min)
+				{
+					best_instr_step[0] = instr;
+					best_instr_step[1] = min;
+				}
+				instr++;
+			}
+			while (--best_instr_step[1])
+			{
+				apply_instr(best_instr_step[0], lst_a, lst_b, 1);
+			}
+			if (!can_push(*lst_a, *lst_b))
+			{
+				best_instr_step[1] = 2147483647;
+				best_instr_step = itm_insert(*lst_a, *lst_b, best_instr_step);
+				while (!can_push(*lst_a, *lst_b))
+					apply_instr(best_instr_step[0], lst_a, lst_b, 1);
+			}
+		}
+		apply_instr(pa, lst_a, lst_b, 1);
 	}
-	free(instr);
+	free(best_instr_step);
 }
--- first_parsing.c
+++ first_parsing.c
@@ -6,7 +6,7 @@
 /*   By: supersko <supersko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 17:24:45 by supersko          #+#    #+#             */
-/*   Updated: 2022/05/23 13:48:03 by supersko         ###   ########.fr       */
+/*   Updated: 2022/05/23 15:32:14 by supersko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,39 @@ void	fl_loop(t_lnk **lst_a, t_lnk **lst_b, int max, int indice_optimisation)
 }
 
 void first_load(t_lnk **lst_a, t_lnk **lst_b, int max)
+{
+	int i;
+	int ind_optimisation;
+
+	i = max;
+	ind_optimisation = 10;
+	if (special_item(*lst_a, max) || (*lst_a)->rank > max / 2)
+	{
+		while (special_item(*lst_a, max) || (*lst_a)->rank > max / 2)
+			apply_instr(ra, lst_a, lst_b, 1);
+	}
+	apply_instr(ra, lst_a, lst_b, 1);
+	while (i--)
+	{
+		fl_loop(lst_a, lst_b, max, ind_optimisation);
+	}
+	//dump on min
+	apply_instr(rrb, lst_a, lst_b, 1);
+	if ((*lst_b)->rank == 0)
+	{
+		apply_instr(pa, lst_a, lst_b, 1);
+		apply_instr(rrb, lst_a, lst_b, 1);
+		apply_instr(pa, lst_a, lst_b, 1);
+	}
+	else
+	{
+		apply_instr(rrb, lst_a, lst_b, 1);
+		apply_instr(pa, lst_a, lst_b, 1);
+		apply_instr(pa, lst_a, lst_b, 1);
+	}
+}
+
+void load_b(t_lnk **lst_a, t_lnk **lst_b, int max)
 {
 	int i;
 	int ind_optimisation;
