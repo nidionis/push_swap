251bdd00853d5ffd5764b7c58faa6b0e41c8bbd2
ndionis <ndionis@k1r3p7.42mulhouse.fr>
2022-05-24 20:27:41 +0200

    libft and libftprintf removed from cache, they can be pushed with push_swap

---
+++ libft/ft_tolower.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: supersko <supersko@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/23 16:48:16 by supersko          #+#    #+#             */
+/*   Updated: 2022/05/15 22:29:05 by supersko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_tolower(int c)
+{
+	if (c >= (int) 'A' && c <= (int) 'Z')
+	{
+		return (c + 32);
+	}
+	else
+	{
+		return (c);
+	}
+}
+/*
+//
+#include "error_msg.h"
+#include <ctype.h>
+#include <stdio.h>
+// MAIN
+int	main(void)
+{
+	int	ii;
+
+	ii = -1;
+	while (ii < 257)
+	{
+		if (tolower(ii) != ft_tolower(ii))
+		{
+			printf("tolower(%c) = %d\n", (char)ii, tolower(ii));
+			printf("ft_tolower(%c) = %d\n", (char)ii, ft_tolower(ii));
+			return (error_msg(ii, "tolower"));
+		}
+		ii++;
+	}
+	printf("You r da man ;)\n");
+	return (0);
+}
+*/
