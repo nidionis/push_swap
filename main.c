81a23c8b76dacc85ee8fae15baf032cebb7342ce
Nicolas Dionis <nidionis@k1r2p7.42mulhouse.fr>
2025-02-09 22:32:19 +0100

    50209-22:28:23-nidionis@k1r2p7.42mulhouse.fr-~/push_swap-(cleaning) => ./complexity 500 50 Démarrage du test : 500 éléments, 50 itérations (seed 3353833442) Pire = 3090 instructions Moyenne = 1440 instructions Meilleur = 203 instructionss Écart-type = 783.0 instructions Objectif = entrez un nombre en troisième argument Précision = entrez un testeur en quatrième argument 100 % effectué Complexity 1.7.1 (2024-11-12)

--- main.c
+++ main.c
@@ -28,6 +28,69 @@
 //	}
 //}
 
+int break_when_minmax_loaded(t_data *d, int instr)
+{
+	(void)instr;
+	if (d->max_b == d->rank_max || d->min_b == 0)
+	{
+		if (d->lst_a->rank == d->rank_max || d->lst_a->rank == 0)
+			return (BREAK_BEST_COMB);
+	}
+	return (TRUE);
+}
+
+int ft_nb_instr(t_list *instr_step_node)
+{
+	int nb_instr;
+	t_instr_step *instr_step;
+
+	nb_instr = 0;
+	while (instr_step_node)
+	{
+		instr_step = instr_step_node->content;
+		nb_instr += instr_step->nb_instr;
+		instr_step_node = instr_step_node->next;
+	}
+	return (nb_instr);	
+}
+
+int first_load_loop(t_data *data)
+{
+	t_list *insertion_step;
+	int nb_instr;
+
+	nb_instr = 0;
+	if (can_load_b(data))
+		apply_instr(data, pb, PRINT);
+	else
+	{
+		insertion_step = ft_best_comb(data, data->r_instr, can_first_load, SIZE_MAX);
+		if (!insertion_step)
+			return (0);
+		nb_instr = ft_nb_instr(insertion_step);
+		if (apply_best_comb_and(break_when_minmax_loaded, data, insertion_step, PRINT) == BREAK_BEST_COMB)
+			return (BREAK_BEST_COMB);
+		ft_lstclear(&insertion_step, free);
+	}
+	return (nb_instr);
+}
+
+int firts_load(t_data *data)
+{
+	int nb_instr;
+	int return_value;
+
+	nb_instr = 0;
+	while (TRUE)
+	{
+		return_value = first_load_loop(data);
+		if (return_value == BREAK_BEST_COMB)
+			break ;
+		nb_instr += return_value;
+	}
+	return (nb_instr);
+}
+
 int	main(int argc, char **argv)
 {
 	t_data d;
@@ -50,22 +113,7 @@ int	main(int argc, char **argv)
 	init_instr_map(instr_map);
 	d.instr_map = instr_map;
 	d.r_instr = instr_ls;
-	while (d.lst_a)
-	{
-		t_list *insertion_step;
-		if (can_load_b(&d))
-			apply_instr(&d, pb, PRINT);
-		else
-		{
-			insertion_step = ft_best_comb(&d, d.r_instr, can_first_load, SIZE_MAX);
-			//print_instr_ls(insertion_step);
-			apply_best_comb_and(NULL, &d, insertion_step, PRINT);
-			if (!insertion_step)
-				break ;
-			ft_lstclear(&insertion_step, free);
-		}
-		//print_lst(&d);
-	}
+	firts_load(&d);
 	//gather_min_and_max(&d);
 	//first_dump(&d);
 	del_lst(&d.lst_a);
