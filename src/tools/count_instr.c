af4ef0ae7c3432b86db7d22248714822b03147e6
Nicolas Dionis <nidionis@k1r4p2.42mulhouse.fr>
2025-04-02 22:15:39 +0200

    [norming]

--- src/tools/count_instr.c
+++ src/tools/count_instr.c
@@ -6,7 +6,7 @@
 /*   By: nidionis <nidionis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 01:36:32 by nidionis          #+#    #+#             */
-/*   Updated: 2025/02/03 01:36:49 by nidionis         ###   ########.fr       */
+/*   Updated: 2025/04/02 22:15:38 by nidionis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ int	count_instr(t_data *data, int instr, int (*can_push)(t_data *data), int max)
 	t_data	data_copy;
 
 	ft_memcpy(&data_copy, data, sizeof(t_data));
-
 	count = 0;
 	while (!can_push(&data_copy) && count < max)
 	{
