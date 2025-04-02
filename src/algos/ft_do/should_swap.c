8898f593535d5c653a7ad2acec887ce3f25cda91
Nicolas Dionis <nidionis@k1r4p2.42mulhouse.fr>
2025-04-02 22:17:26 +0200

    [norming]

--- src/algos/ft_do/should_swap.c
+++ src/algos/ft_do/should_swap.c
@@ -6,43 +6,34 @@
 /*   By: nidionis <nidionis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 17:24:45 by supersko          #+#    #+#             */
-/*   Updated: 2025/02/09 16:30:59 by nidionis         ###   ########.fr       */
+/*   Updated: 2025/04/02 22:17:00 by nidionis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int should_swap_b_unsafe(t_lnk *lst)
+int	should_swap_b_unsafe(t_lnk *lst)
 {
-    t_lst temp;
-    
-    temp.lst = lst;
-    return (head(&temp) < next(&temp) && head(&temp) != get_max(lst)->rank);
+	t_lst	temp;
+
+	temp.lst = lst;
+	return (head(&temp) < next(&temp) && head(&temp) != get_max(lst)->rank);
 }
 
-int should_swap_b(t_lnk *lst, int mediane)
+int	should_swap_b(t_lnk *lst, int mediane)
 {
-    int nb;
-    int n;
-    (void)mediane;
+	int		nb;
+	int		n;
+	t_lst	temp;
 
-    t_lst temp;
-    
-    temp.lst = lst;
-    nb = head(&temp);
-    n = next(&temp);
-    if (!lst || ft_dlstsize(lst) == 1)
-        return (FALSE);
-    //if (nb < mediane && n > mediane)
-    //    return (FALSE);
-    if (nb > n)
-        if (nb != get_max(lst)->rank || n != get_min(lst)->rank)
-        return (TRUE);
-    return (FALSE);
+	(void)mediane;
+	temp.lst = lst;
+	nb = head(&temp);
+	n = next(&temp);
+	if (!lst || ft_dlstsize(lst) == 1)
+		return (FALSE);
+	if (nb > n)
+		if (nb != get_max(lst)->rank || n != get_min(lst)->rank)
+			return (TRUE);
+	return (FALSE);
 }
-
-//int should_swap_b_(t_data *data, int instr)
-//{
-//    (void)instr;
-//    return (should_swap_b(data->lst_b));
-//}
